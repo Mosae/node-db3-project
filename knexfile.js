44e5579686c721ae71685bf4df7c7519afb7f9fd
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-14 15:21:35 -0500

    data base works

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,6 @@
 module.exports = {
 	development: {
 		client: 'sqlite3',
-		useNullAsDefault: true, // needed for sqlite
 		connection: {
 			filename: './data/schemes.db3',
 		},
@@ -12,13 +11,6 @@ module.exports = {
 		},
 		seeds: {
 			directory: './data/seeds',
-		},
-		// add the following
-		pool: {
-			afterCreate: (conn, done) => {
-				// runs after a connection is made to the sqlite engine
-				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-			},
 		},
 	},
 };

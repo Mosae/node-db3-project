252b7c6cf4fd3c5410795d765dc02e06d1e47ee0
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-05-14 09:56:27 -0500

    Query practice done - fixed 1st one

--- queries.sql
+++ queries.sql
@@ -1,14 +1,12 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-SELECT products.ProductName as product, categories.CategoryName as category FROM [Products]
-INNER JOIN Categories 
+SELECT categoryID, productname FROM [Products]
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT orders.OrderId, Shippers.ShipperName, orders.orderDate FROM [Orders]
 INNER JOIN Shippers
 where orders.OrderDate 
-
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT ProductName, OrderId, Unit FROM [Products]
